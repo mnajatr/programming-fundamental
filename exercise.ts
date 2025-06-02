62fad8e46d5acea0308ac3b7a4241b51d22fbea1
Naja <trirazaqa@gmail.com>
2025-06-02 10:57:56 +0700

    feat: add convert cm to km

--- exercise.ts
+++ exercise.ts
@@ -14,16 +14,24 @@
 // QUESTION 2: Write a code to check whether a string is a palindrome or not.
 // (Tulis kode untuk memeriksa apakah suatu string adalah palindrom atau bukan.)
 
-let str: string = "kodok";
-let normalStr: string = str.toLowerCase();
-let reverseStr: string = "";
+// let str: string = "kodok";
+// let normalStr: string = str.toLowerCase();
+// let reverseStr: string = "";
 
-for (let i = normalStr.length - 1; i >= 0; i--) {
-  reverseStr += normalStr[i];
-}
+// for (let i = normalStr.length - 1; i >= 0; i--) {
+//   reverseStr += normalStr[i];
+// }
 
-if (normalStr === reverseStr) {
-  console.log(`${str} adalah palindrom`);
-} else {
-  console.log(`${str} bukan palindrom`);
-}
+// if (normalStr === reverseStr) {
+//   console.log(`${str} adalah palindrom`);
+// } else {
+//   console.log(`${str} bukan palindrom`);
+// }
+
+//////////////////////////////
+// QUESTION 3: Write a code to convert centimeter to kilometer.
+// (Tulis kode untuk mengubah sentimeter ke kilometer.)
+
+const cm: number = 500000;
+
+console.log(`${cm} CM -> ${cm / 100000} KM`);
