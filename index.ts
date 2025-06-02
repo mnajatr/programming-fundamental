99e8716caf53cfc093fd7a71fb7ba1bd84fdd838
Naja <trirazaqa@gmail.com>
2025-06-02 10:11:08 +0700

    new branch conditional and loop statements

--- index.ts
+++ index.ts
@@ -1,77 +1,157 @@
 /*
-//////////////////////////////////////////////
-// Getting started with Typescript
-let x: number = 10;
-let y: string = "Hello World!";
-const message: string = "I Love JavaScript";
+//////////////////////////////////////
+// Conditional and loop statements //
+/////////////////////////////////////
+let age: number = 21;
 
-//////////////////////////////////////////////
-// String Built-in Method
-console.log(x, y, message);
-console.log(message.slice(2, 6));
-console.log(message.toLowerCase());
-console.log(message.replace("Love", "Hate"));
+/////////////////////////////////
+// If statement
+if (age >= 17) {
+  console.log("You can now create an ID Card");
+}
 
-//////////////////////////////////////////////
-// Template Literals
-const name: string = "Naja";
-const age: number = 20;
-const message: string = `My name is ${name}, ${age}-years old`;
+/////////////////////////////////
+// Else statement
+if (age >= 17) {
+  console.log("You can now create an ID Card");
+} else {
+  console.log("You are not old enough to create an ID Card");
+}
 
-console.log(message);
+/////////////////////////////////
+// Else if statement & Chaining conditions
+let grade: string = "A";
 
-//////////////////////////////////////////////
-// Number Built-in Method
-const number: number = 10;
+if (grade == "A") {
+  console.log("Excellent result!");
+} else if (grade == "B") {
+  console.log("Great result!");
+} else if (grade == "C") {
+  console.log("Average result!");
+} else {
+  console.log("Invalid grade");
+}
 
-console.log(number.toString());
+/////////////////////////////////
+// Switch Case
+let now: Date = new Date();
+let day: number = now.getDay(); // 0 - 6
 
-//////////////////////////////////////////////
-// Date Data Type
-const now: Date = new Date();
+switch (day) {
+  case 0:
+    console.log("Minggu");
+    break;
+  case 1:
+    console.log("Senin");
+    break;
+  case 2:
+    console.log("Selasa");
+    break;
+  case 3:
+    console.log("Rabu");
+    break;
+  case 4:
+    console.log("Kamis");
+    break;
+  case 5:
+    console.log("Jumat");
+    break;
+  case 6:
+    console.log("Sabtu");
+    break;
+}
 
-console.log(now);
-console.log(now.getFullYear());
-console.log(now.getMonth());
-console.log(now.getDay());
+/////////////////////////////////
+// Truthy and falsy values
+let word = "";
 
-//////////////////////////////////////////////
-// Basic Operators
-const x: number = 5;
-const y: number = 2;
+if (word) {
+  console.log("Hello World!");
+}
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x ** y);
+// console.log(Number("12a"));
 
-//////////////////////////////////////////////
-// Unary, Binary and Operand
-let x: number = 1;
-x = -x;
+/////////////////////////////////
+// Logical Operators
+let x: number = 6;
+let y: number = 3;
 
-console.log(x);
+if (x < 10 || y > 1) {
+  // console.log("Hello World!");
+}
 
-//////////////////////////////////////////////
-// Modify in Place
-let n: number = 2;
-n += 5;
-n *= 2;
+// console.log(!(x == 6));
 
-console.log(n);
+const gender: string = "F";
+if (gender == "M") {
+  console.log("Male");
+} else {
+  console.log("Female");
+}
 
-//////////////////////////////////////////////
-// Increment & Decrement
-let count: number = 2;
-count -= 3;
-count--;
-console.log(++count);
+/////////////////////////////////
+// Ternary Operator
+const str: string = "Typescript";
 
-//////////////////////////////////////////////
-// Postfix & Prefix Form
-let preCounter: number = 0;
-console.log(preCounter++);
-console.log(++preCounter);
+console.log(str === "Typescript" ? "Benar" : "Salah");
+
+/////////////////////////////////
+// Short-Circuiting
+const value: string = "Andi";
+const name: string = value && "Budi"; // Jika value "ada" maka log Budi
+// const name: string = value || "Budi"; // Jika value "" maka log value
+
+console.log(name);
+
+
+/////////////////////////////////
+// Loop statements
+for (let i: number = 1; i <= 10; i++) {
+  console.log(`Hello World ke ${i}`);
+  if (i == 5 i) continue;
+}
+
+for (let i: number = 10; i >= 1; i--) {
+  console.log(`Hello World ke ${i}`);
+}
+
+/////////////////////////////////
+// While loop
+let i: number = 10;
+while (i >= 1) {
+  console.log(`Hello world ke-${i}`);
+  i--;
+}
+
+/////////////////////////////////
+// Do … While loop
+let i: number = 1;
+do {
+  console.log(`Hello World ke-${i}`);
+  i++;
+} while (i <= 10);
+
+/////////////////////////////////
+// Break & Continue
+let sum: number = 1;
+
+while (true) {
+  console.log(sum);
+  if (sum == 10) break;
+  sum++;
+}
+
+for (let i: number = 1; i <= 10; i++) {
+  console.log(`Hello World ke ${i}`);
+  if (i == 5 || i == 9) continue;
+}
+
+for (let i = 1; i <= 10; i++) {
+  // console.log("*".repeat(i));
+  let bintang = "";
+  for (let j = 1; j <= i; j--) {
+    bintang += "*";
+  }
+  console.log(bintang);
+}
 */
