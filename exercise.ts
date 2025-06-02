64bbd0b06a406448d5f84fd024e684ff6b6b3c3f
Naja <trirazaqa@gmail.com>
2025-06-02 10:48:48 +0700

    feat: add palindrome

--- exercise.ts
+++ exercise.ts
@@ -2,10 +2,28 @@
 // QUESTION 1: Write a code to display the multiplication table of a given integer.
 // (Tulis kode untuk menampilkan tabel perkalian bilangan bulat yang diberikan.)
 
-let input: number = 9;
-const limit: number = 10;
+// let input: number = 9;
+// const limit: number = 10;
 
-console.log(`Tabel perkalian untuk: ${input}`);
-for (let i: number = 1; i <= limit; i++) {
-  console.log(`${input} x ${i}: ${input * i}`);
+// console.log(`Tabel perkalian untuk: ${input}`);
+// for (let i: number = 1; i <= limit; i++) {
+//   console.log(`${input} x ${i}: ${input * i}`);
+// }
+
+//////////////////////////////
+// QUESTION 2: Write a code to check whether a string is a palindrome or not.
+// (Tulis kode untuk memeriksa apakah suatu string adalah palindrom atau bukan.)
+
+let str: string = "kodok";
+let normalStr: string = str.toLowerCase();
+let reverseStr: string = "";
+
+for (let i = normalStr.length - 1; i >= 0; i--) {
+  reverseStr += normalStr[i];
+}
+
+if (normalStr === reverseStr) {
+  console.log(`${str} adalah palindrom`);
+} else {
+  console.log(`${str} bukan palindrom`);
 }
